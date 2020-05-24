8ea2082cbc5268192edf7c49317d5f5229544894
LAPTOP-O0BUM6SD\Livio <livio.guidotti@libero.it>
2020-05-25 01:53:33 +0200

    Plot for all 4 state's variables

--- predict.py
+++ predict.py
@@ -4,6 +4,26 @@ import matplotlib.pyplot as plt
 
 from neural_network.physics_neural_network import physical_nn
 
+def plot_results(realcartpos,predcartpos,realcartvel,predcartvel,realtheta,predtheta,realomega,predomega):
+
+
+    f, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+    ax1.plot(realcartpos, 'b', label='Real pos')
+    ax1.plot(predcartpos, 'r:', label='Pred pos')
+    ax1.legend(loc="upper right")
+    ax2.plot(realcartvel, 'b', label='Real vel')
+    ax2.plot(predcartvel, 'r:', label='Pred vel')
+    ax2.legend(loc="upper right")
+    ax3.plot(realtheta, 'b', label='Real theta')
+    ax3.plot(predtheta, 'r:', label='Pred theta')
+    ax3.legend(loc="upper right")
+    ax4.plot(realomega, 'b', label='Real omega')
+    ax4.plot(predomega, 'r:', label='Pred omega')
+    ax4.legend(loc="upper right")
+
+    plt.show()
+
+
 if __name__ == "__main__":
     model = physical_nn("neural_network/cart_pole_nn_saved")
     
@@ -20,36 +40,53 @@ if __name__ == "__main__":
         img = np.array([model.x_test[n]])
         # print("testiamo l'input")
         # print(img)
-        tempreal.append(img)
+        tempreal.append(np.array([model.y_test[n]]))
 
         prediction = model.predict(img)
-
         temppred.append(prediction)
 
         #print('Original: {} \nPredicted: {}'.format(img, prediction))
-    realvalues = []
-    predvalues = []
+    realcartpos = []
+    predcartpos = []
+    realcartvel = []
+    predcartvel = []
+    realtheta = []
+    predtheta = []
+    realomega = []
+    predomega = []
     for n in range(len(model.x_test)):
-        realvalues.append(tempreal[n][0,3])
-        predvalues.append(temppred[n][0,2])
-    # plt.plot(realvalues)
-    # plt.ylabel('values')
+        realcartpos.append(tempreal[n][0,0])
+        predcartpos.append(temppred[n][0,0])
+        realcartvel.append(tempreal[n][0,1])
+        predcartvel.append(temppred[n][0,1])
+        realtheta.append(tempreal[n][0,2])
+        predtheta.append(temppred[n][0,2])
+        realomega.append(tempreal[n][0,3])
+        predomega.append(temppred[n][0,3])
+
+
+    #plot_results(realcartpos,predcartpos, realcartvel, predcartvel, realtheta, predtheta, realomega, predomega)
+
+    # plt.figure()
+
+    # plt.subplot(2,2,1)
+    # plt.plot(realcartpos, '-b', label='Real pos')
+    # plt.plot(predcartpos, '-r', label="Pred pos")
+
+    # plt.subplot(2,2,2)
+    # plt.plot(realcartvel)
+    # plt.plot(predcartvel)
+ 
+    # plt.subplot(2,2,3)
+    # plt.plot(realtheta)
+    # plt.plot(predtheta)
+
+    # plt.subplot(2,2,4)
+    # plt.plot(realomega)
+    # plt.plot(predomega)
+
     # plt.show()
 
-    # plt.plot(predvalues)
-    # plt.ylabel('values')
-    # plt.show()
-
-    plt.figure()
-
-    plt.subplot(211)
-    plt.plot(realvalues)
-
-    plt.subplot(212)
-    plt.plot(predvalues)
-    plt.show()
-
-
-    # fig, ax = plt.subplots()
-    # ax.plot(realvalues)
-    # ax.set_title('A single plot')
+    
+    plot_results(realcartpos,predcartpos,realcartvel,predcartvel,realtheta,predtheta,realomega,predomega)
+    
