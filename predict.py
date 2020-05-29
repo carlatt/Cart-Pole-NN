80bec7801e9d6b71b6cb9a4a0e4e2eb9862c4831
LAPTOP-O0BUM6SD\Livio <livio.guidotti@libero.it>
2020-05-29 18:07:14 +0200

    correct predict.py

--- predict.py
+++ predict.py
@@ -1,25 +1,8 @@
-import matplotlib.pyplot as plt
 import numpy as np
-
+from plot import plot_errors
+from plot import plot_results
 from neural_network.physics_neural_network import physical_nn
 
-
-def plot_results(realcartpos, predcartpos, realcartvel, predcartvel, realtheta, predtheta, realomega, predomega):
-    f, (ax1, ax2, ax3, ax4) = plt.subplots(4)
-    ax1.plot(realcartpos, 'b', label='Real pos')
-    ax1.plot(predcartpos, 'r:', label='Pred pos')
-    ax1.legend(loc="upper right")
-    ax2.plot(realcartvel, 'b', label='Real vel')
-    ax2.plot(predcartvel, 'r:', label='Pred vel')
-    ax2.legend(loc="upper right")
-    ax3.plot(realtheta, 'b', label='Real theta')
-    ax3.plot(predtheta, 'r:', label='Pred theta')
-    ax3.legend(loc="upper right")
-    ax4.plot(realomega, 'b', label='Real omega')
-    ax4.plot(predomega, 'r:', label='Pred omega')
-    ax4.legend(loc="upper right")
-
-    plt.show()
 
 
 if __name__ == "__main__":
@@ -30,16 +13,17 @@ if __name__ == "__main__":
     model.load_data("./simulation_data/U" + str(i) + ".csv",
                     "./simulation_data/Y" + str(i) + ".csv")
 
-    tempreal = []
-    temppred = []
-    for n in range(len(model.x)):
-        img = np.array([model.x[n]])
-        tempreal.append(np.array([model.y[n]]))
+    # tempreal = []
+    # temppred = []
+    # for n in range(len(model.x)):
+    #     img = np.array([model.x[n]]) 
+    #     tempreal.append(np.array([model.y[n]]))
 
-        prediction = model.predict(img)
-        temppred.append(prediction)
+    #     prediction = model.predict(img)
+    #     temppred.append(prediction)
 
-
+    Y = model.y
+    pred = model.predict()
 
     realcartpos = []
     predcartpos = []
@@ -50,18 +34,18 @@ if __name__ == "__main__":
     realomega = []
     predomega = []
     for n in range(len(model.x)):
-        realcartpos.append(tempreal[n][0, 0])
-        predcartpos.append(temppred[n][0, 0])
-        realcartvel.append(tempreal[n][0, 1])
-        predcartvel.append(temppred[n][0, 1])
-        realtheta.append(tempreal[n][0, 2])
-        predtheta.append(temppred[n][0, 2])
-        realomega.append(tempreal[n][0, 3])
-        predomega.append(temppred[n][0, 3])
+        realcartpos.append(Y[n][0])
+        predcartpos.append(pred[n][0])
+        realcartvel.append(Y[n][1])
+        predcartvel.append(pred[n][1])
+        realtheta.append(Y[n][2])
+        predtheta.append(pred[n][2])
+        realomega.append(Y[n][3])
+        predomega.append(pred[n][3])
 
     plot_results(realcartpos, predcartpos, realcartvel, predcartvel, realtheta, predtheta, realomega, predomega)
-    Y = model.y
-    T = model.predict()
-    nMSE = np.power(T - Y, 2) / np.power(T, 2)
-    #nMSE = ((T - Y) ** 2) / (T ** 2)
-    plt.plot(nMSE)
+    
+    error = Y - pred
+    
+    plot_errors(error)
+    
